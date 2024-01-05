a9d1a250cbc061fbf7ab8f148fc94a0a81f1cca0
kipkoech88 <hoseakipkoech88@gmail.com>
2024-01-05 08:47:46 -0500

    added file to create processes and exec system calls to execute commands

---
+++ shell_launch.c
@@ -0,0 +1,40 @@
+#include <unistd.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * shell_launch - launches the shell instance and execute commands
+ * @args: arguments 
+ * Return: 1
+ */
+
+int shell_launch(char **args)
+{
+	pid_t pid, wpid;
+	int status;
+
+	pid = fork();
+	if (pid == 0)
+	{
+		//child process
+		if ( execvp(args[0], args) == -1)
+		{
+			perror("shell\n");
+		}
+		exit(EXIT_FAILURE);
+	}
+	else if (pid < 0)
+	{
+		//Error creating child process/forking
+		perror("Shell");
+	}
+	else
+	{
+		do{
+			//parent process
+			wpid = waitpid(pid, &status, WUNTRACED);
+		} while (!WIFEXITED(status) && !WIFSIGNALED(status));
+	}
+
+	return (1);
+}
